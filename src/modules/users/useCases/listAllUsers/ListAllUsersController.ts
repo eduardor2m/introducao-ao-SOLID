6e35dbf935d7aef314d3abd440b26e635ca2a034
eduardor2m <deveduardomelo@gmail.com>
2022-03-23 03:15:05 -0300

    feat: desafio concluido

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prefer-destructuring */
 import { Request, Response } from "express";
 
 import { ListAllUsersUseCase } from "./ListAllUsersUseCase";
@@ -6,7 +7,26 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    try {
+      const { user_id } = request.params;
+
+      if (!user_id) {
+        const user_id_header = request.headers.user_id;
+
+        const users = this.listAllUsersUseCase.execute({
+          user_id: user_id_header.toString(),
+        });
+
+        return response.status(200).json(users);
+      }
+      const users = this.listAllUsersUseCase.execute({ user_id });
+
+      return response.status(200).json(users);
+    } catch (error) {
+      return response.status(400).json({
+        error: error.message,
+      });
+    }
   }
 }
 
